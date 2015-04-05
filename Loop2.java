f0844c72a063b7d1c43d5cc586553af464cfcf91
ago26 <ago26@email.vccs.edu>
2015-04-05 14:22:35 -0400

    Odd Number Loop

---
+++ Loop2.java
@@ -0,0 +1,18 @@
+
+
+public class Loop2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		while (true){
+			for (int i=1; i<100; i=i+2)
+				if (i % 2 !=0)
+			        System.out.println(i);
+		}
+		
+
+	}
+
+}
+
